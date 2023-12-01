a7f42b6949513a11eba7660a326ba7110a63200a
CriminalChicken <y.meichtry@gmx.ch>
2023-12-01 07:52:49 +0100

    added header and footer

--- src/header/header.js
+++ src/header/header.js
@@ -3,32 +3,26 @@ import "./header.css";
 
 function Header({ toggleMenu, isToggled }) {
   return (
-    <header>
-      <div className={`burger-menu ${isToggled ? "show-menu" : ""}`}>
-        <div className="burger-icon" onClick={toggleMenu}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
-        <ul className="menu-list">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
+    <header className="header">
+      <div className="burger-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
-      <div className="content">
-        <h1>Welcome to Our Website</h1>
-        <p>This is a simple example of a burger menu.</p>
-      </div>{" "}
+      <ul className={`menu-list ${isToggled ? "show-menu" : ""}`}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
     </header>
   );
 }
