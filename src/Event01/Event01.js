1c25998d54e8e46d528260a45664f3f28f57c691
CriminalChicken <y.meichtry@gmx.ch>
2024-01-12 10:41:01 +0100

    finished Event01

--- src/Event01/Event01.js
+++ src/Event01/Event01.js
@@ -1,11 +1,41 @@
 // Event01.js
 import React from "react";
+import "./Event01.css"; // Stellen Sie sicher, dass Sie die richtige CSS-Datei importieren
+
+const BookingTimeBox = ({ time }) => {
+  return (
+    <div className="booking-time-box">
+      <p>{time}</p>
+      <button>Buchen</button>
+    </div>
+  );
+};
 
 const Event01 = () => {
   return (
     <div>
       <h1>Zoo-Besuch mit Jugendlichen</h1>
-      <p>Hier können Sie weitere Informationen zum Zoo-Besuch mit Jugendlichen anzeigen.</p>
+      
+      <div className="booking-day">
+        <h2>Montag, Dienstag:</h2>
+        <BookingTimeBox time="10:00-12:00" />
+        <BookingTimeBox time="14:00-16:00" />
+        <BookingTimeBox time="17:00-19:00" />
+      </div>
+
+      <div className="booking-day">
+        <h2>Donnerstag, Freitag:</h2>
+        <BookingTimeBox time="09:00-11:00" />
+        <BookingTimeBox time="13:00-15:00" />
+        <BookingTimeBox time="17:00-19:00" />
+      </div>
+
+      <div className="booking-day">
+        <h2>Samstag, Sonntag:</h2>
+        <BookingTimeBox time="11:00-13:00" />
+        <BookingTimeBox time="14:00-16:00" />
+        <BookingTimeBox time="18:00-20:00" />
+      </div>
     </div>
   );
 };
