567a860a3214c38b13cc0a924ef0a4c724290cf0
CriminalChicken <y.meichtry@gmx.ch>
2024-01-12 08:31:39 +0100

    new /home

--- src/Home/home.js
+++ src/Home/home.js
@@ -3,22 +3,44 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Link, Switch, Router } from "react-router-dom"; // Füge die notwendigen Imports hinzu
 import "./home.css";
 
-function Home() {
-
+const Home = () => {
     return (
-        <Router>
-        <div className="content">
-            <h1>Welcome to Our Website</h1>
-            <p>This is a simple example of a burger menu.</p>
-        </div>{ " " }
-    <div className="button-container">
-        <Link to="/">Home</Link>
-        <Link to="/events">Events</Link>
-        <Link to="/kaufen">Kaufen</Link>
-        <Link to="/payFormular">Tickets</Link>
-    </div>
-    </Router>
-  );
-}
-
-export default Home;
+      <div>
+        <h1>Willkommen im Zoo!</h1>
+  
+        <div>
+          <h2>Öffnungszeiten:</h2>
+          <p>
+            Montag-Freitag: 09:00-18:00
+            <br />
+            Samstag-Sonntag: 09:00-20:00
+          </p>
+        </div>
+  
+        <div>
+          <h2>Preise:</h2>
+          <p>
+            Kinder: 10.-
+            <br />
+            Erwachsene: 14.-
+            <br />
+            Senioren: 10.-
+            <br />
+            Jugendliche: 12.-
+          </p>
+        </div>
+  
+        <div>
+          <h2>Events:</h2>
+          <ul>
+            <li>Zoo-Besuch mit Jugendlichen</li>
+            <li>Bastel-Workshop</li>
+            <li>Tiere füttern mit Senioren</li>
+            <li>Meditieren mit den Tieren</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Home;
